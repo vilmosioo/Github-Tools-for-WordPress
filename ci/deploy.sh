86d0f72307e42aeac124ecd8f9be37e5c3620494
Vilmos Ioo <cool.villi@gmail.com>
2015-05-14 01:09:12 +0100

    Fetching

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,10 +1,9 @@
 # Updates svn branch with lastest
 
-git svn -h
 git fetch
 git checkout master
 git pull origin master
-git checkout svnsync
+git checkout -b svnsync origin/svnsync
 svn update
 git merge --no-ff master
 git commit
