43a9c27e8513acb2286906e49a9f683355151dd2
Vilmos Ioo <vioo@palantir.com>
2015-05-15 15:45:06 +0100

    Testing

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,15 +7,15 @@ git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*" # this is n
 git fetch
 git stash
 git checkout svnsync
-svn update
+#svn update
 git merge --no-ff --no-edit master
 git stash pop
 git add . # add the modified files from the build process
-git commit
-svn commit
-git push origin svnsync
+git commit -m "Updated version references"
+#svn commit
+#git push origin svnsync
 
 echo 'Publishing master'
 git checkout master
-git push origin master
-git push origin master --tags
+#git push origin master
+#git push origin master --tags
