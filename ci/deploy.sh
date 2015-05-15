ba33ede50a609c5f58ac1c0414e092ad77f7223b
Vilmos Ioo <vioo@palantir.com>
2015-05-15 14:32:28 +0100

    Stashing issues first

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,9 +5,11 @@ set -e
 echo 'Publishing svnsync'
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*" # this is necessary to allow the repo to fetch other remote branches
 git fetch
+git stash
 git checkout svnsync
 svn update
 git merge --no-ff master
+git stash pop
 git add . # add the modified files from the build process
 git commit
 svn commit
