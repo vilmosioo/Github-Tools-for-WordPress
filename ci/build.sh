6b586a768fb900ca4df9faa96d4f6d0a0c6ed377
Vilmos Ioo <cool.villi@gmail.com>
2015-08-03 21:34:39 +0100

    Git set up

--- ci/build.sh
+++ ci/build.sh
@@ -6,9 +6,7 @@ echo 'Switching to master and setting identity for git'
 git checkout master
 git config user.name $GIT_NAME
 git config user.email $GIT_EMAIL
-git config credential.helper "store --file=.git/credentials"
-git config remote.origin.url https://github.com/vilmosioo/Github-Tools-for-WordPress.git
-echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+git remote set-url origin git@github.com:vilmosioo/Github-Tools-for-WordPress.git
 
 echo 'Patching version...'
 grunt bump-only:patch
